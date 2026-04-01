365dbb47d8138cdf10e4e0c79fed0cfce9e512c4
Gerihmen <105604591+Gerihmen@users.noreply.github.com>
2026-04-01 19:51:45 +0300

    Update Bread-Hub.lua

--- Bread-Hub.lua
+++ Bread-Hub.lua
@@ -3,7 +3,7 @@ local placeId = game.PlaceId
 if placeId == 126323186948264 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Gerihmen/Paradox/refs/heads/main/Main.lua"))()
 else
-  local msg = Instance.new("Message")
+	local msg = Instance.new("Message")
 	msg.Text = "No."
 	msg.Parent = workspace
 	task.wait(5)
